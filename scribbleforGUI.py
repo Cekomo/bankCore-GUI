b4c02e502bd2af56571bfc65613850e2f0fd4f1c
Cemil Şahin <cemil.sahin@ozu.edu.tr>
2021-09-08 10:24:39 +0300

    signin window implementation & connection

--- scribbleforGUI.py
+++ scribbleforGUI.py
@@ -19,11 +19,6 @@ class Window2(QMainWindow):
 
         self.txt = QtWidgets.QTextBrowser(self)
         W.textbrowser(self.txt, "Please type your identity number and password.", 10, 125, 100, 350, 50)
-        # self.txt.setReadOnly(1)
-        # self.txt.setText("Please type your identity number and password.")
-        # self.txt.setFont(QFont("Arial", 10))
-        # self.txt.move(125, 100).
-        # self.txt.resize(350, 50).
 
         self.idlabel = QtWidgets.QLabel(self) 
         W.label(self.idlabel, "Identity Number:", 75, 210, 120, 25)
@@ -55,86 +50,84 @@ class Window2(QMainWindow):
         self.hide()
 
 
-"""
-def loginWindow(self): # login window when pressed login
-    # win.setWindowTitle("bankCore version(2.5)")
-    # win.setGeometry(200, 200, 600, 600)
+class Window3(QMainWindow):
+    def __init__(self):
+        super().__init__()
 
-    self.txt = QtWidgets.QTextBrowser("Please type your identity number and password.", self)
-    self.txt.setText("Please type your identity number and password.")
-    self.txt.setFont(QFont("Arial", 10))
-    self.txt.move(125, 100)
-    self.txt.resize(350, 50)
+        W = Window()
 
-    self.idlabel = QtWidgets.QLabel("Identity Number: ", self) # string intersects with the box
-    self.idlabel.resize(120, 25) # idlabel.setFixedSize(120, 30) # --> an alternative
-    self.idlabel.setFont(QFont("Arial", 10))
-    self.idlabel.move(75, 210)
+        self.setWindowTitle("bankCore version(2.5)")
+        self.setGeometry(200, 200, 600, 600)
 
-    self.idtext = QtWidgets.QLineEdit(self)
-    self.idtext.setMaxLength(11)
-    self.idtext.setFont(QFont("Arial", 18))
-    self.validator = QDoubleValidator()
-    self.idtext.setValidator(self.validator)
-    # validator.setRange(10000000000, 99999999999) # try to fix this
-    # input only accepts integer values, try to make it 11 digits limitation for the box
-    self.idtext.move(200, 200)
-    self.idtext.resize(200, 50)
+        self.txt = QtWidgets.QTextBrowser(self)
+        W.textbrowser(self.txt, "Please type the informations below.", 10, 125, 100, 350, 50)
 
-    self.pswlabel = QtWidgets.QLabel("Password:", self)
-    self.pswlabel.setText("Password:") # string intersects with the box
-    self.pswlabel.resize(80, 25) # idlabel.setFixedSize(120, 30) # --> an alternative
-    self.pswlabel.setFont(QFont("Arial", 10))
-    self.pswlabel.move(113, 285)
+        self.namelabel = QtWidgets.QLabel(self) 
+        W.label(self.namelabel, "Name:", 85, 210, 120, 25)
 
-    self.pswtext = QtWidgets.QLineEdit(self)
-    self.pswtext.setMaxLength(15)
-    self.pswtext.setFont(QFont("Arial", 18))
-    # pswtext.setText("")
-    # validator = QValidator()
-    # pswtext.setValidator(validator)
-    self.pswtext.move(200, 275)
-    self.pswtext.resize(200, 50)
+        self.nameline = QtWidgets.QLineEdit(self)
+        W.linedit(self.nameline, 200, 200, 200, 50, 13)
 
-    self.logButton = QtWidgets.QPushButton("Log In", self)
-    self.logButton.move(200, 350)
-    self.logButton.resize(200, 50)
+        self.snamelabel = QtWidgets.QLabel(self) 
+        W.label(self.snamelabel, "Surname:", 85, 266, 120, 25)
 
-    self.returnButton = QtWidgets.QPushButton("Return", self)
-    self.returnButton.move(200, 425)
-    self.returnButton.resize(200, 50)
+        self.snameline = QtWidgets.QLineEdit(self)
+        W.linedit(self.snameline, 200, 256, 200, 50, 13) # for surname, 13 char may not be sufficient
 
-    # txt.setText(f"Logged in.") # if process is successful, this text will be shown
-    self.win.show()
+        self.idlabel = QtWidgets.QLabel(self) 
+        W.label(self.idlabel, "TCK Number:", 85, 322, 120, 25)
 
-    # sys.exit(app.exec_())
-"""
+        self.idline = QtWidgets.QLineEdit(self)
+        W.linedit(self.idline, 200, 312, 200, 50, 11)
 
-class Window(QMainWindow):
+        self.passwlabel = QtWidgets.QLabel(self) 
+        W.label(self.passwlabel, "Password:", 85, 378, 120, 25)
 
+        self.passwline = QtWidgets.QLineEdit(self)
+        W.linedit(self.passwline, 200, 368, 200, 50, 15) # for password, 13 char may not be sufficient
+
+        self.signinButton = QtWidgets.QPushButton(self)
+        W.button(self.signinButton, "Sign In", 199, 425, 100, 50) # this must send variables to sql if variables are valid
+
+        self.returnButton = QtWidgets.QPushButton(self)
+        W.cbutton(self.returnButton, "Return", 301, 425, 100, 50, self.returnWindow)
+
+    def returnWindow(self):
+        self.a = Window()
+        self.a.show()
+        self.hide()
+
+
+class Window4(QMainWindow):
     def __init__(self):
         super().__init__()
 
         self.setWindowTitle("bankCore version(2.5)")
         self.setGeometry(200, 200, 600, 600)
 
-        self.txt = QtWidgets.QTextEdit("Welcome to bankCore!", self)
-        self.txt.setReadOnly(1)
-        self.txt.setFont(QFont("Arial", 20))
-        self.txt.move(125, 100)
-        self.txt.resize(350, 50)
+
+
+class Window(QMainWindow):
+    def __init__(self):
+        super().__init__()
+
+        self.setWindowTitle("bankCore version(2.5)")
+        self.setGeometry(200, 200, 600, 600)
+
+        self.txt = QtWidgets.QTextBrowser(self)
+        self.textbrowser(self.txt, "Welcome to bankCore!", 20, 125, 100, 350, 50)
 
         self.logButton = QPushButton(self)
         self.cbutton(self.logButton, "Log In", 200, 200, 200, 50, self.loginWindow)
 
         self.signButton = QtWidgets.QPushButton(self)
-        self.button(self.signButton, "Sign In", 200, 275, 200, 50)
+        self.cbutton(self.signButton, "Sign In", 200, 275, 200, 50, self.signWindow)
 
         self.aboutButton = QtWidgets.QPushButton(self)
-        self.button(self.aboutButton, "About bankCore", 200, 350, 200, 50)
+        self.cbutton(self.aboutButton, "About bankCore", 200, 350, 200, 50, self.aboutWindow)
 
         self.exitButton = QtWidgets.QPushButton(self)
-        self.button(self.exitButton, "Exit", 200, 425, 200, 50)
+        self.cbutton(self.exitButton, "Exit", 200, 425, 200, 50, self.exitWindow)
 
         self.show()
     
@@ -142,6 +135,21 @@ class Window(QMainWindow):
         self.w = Window2()
         self.w.show()
         self.hide()
+
+    def signWindow(self):
+        self.s = Window3()
+        self.s.show()
+        self.hide()
+
+    def aboutWindow(self):
+        self.a = Window4()
+        self.a.show()
+        self.hide()
+
+    
+    def exitWindow(self):
+        self.close() # I do NOT know if that is the solution
+        # the code is terminating due to closure of the window
 
     def timer(self, x):
         QtTest.QTest.qWait(x)
